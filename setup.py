cc5b81e383109f75b95636804479665a9091ddc2
mulan-94 <andatilexy@gmail.com>
2019-02-08 13:51:42 +0200

    Bump up version number to 0.0.4 in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def requirements():
         return [pname.strip() for pname in f.readlines()]
 
 setup(name='ragavi',
-      version="0.0.3",
+      version="0.0.4",
       description='Radio Astronomy Gain and Visibility Inspector',
       long_description=readme(),
       url='https://github.com/ratt-ru/ragavi',
