cdba38cf716b97cb2331ad083f96e9c19889a830
Simon Perkins <simon.perkins@gmail.com>
2019-02-19 13:13:16 +0200

    Add second example demonstrating dim reduction

--- examples/avg_example.py
+++ examples/avg_example.py
@@ -19,12 +19,15 @@ xds = list(xds_from_ms(args.ms, chunks={"row": args.rows}))
 
 
 def fn(data):
+    """ Because we're not reducing over a dimension,
+    blockwise passes numpy arrays to fn """
     return np.average(data, axis=1)[:, None, :]
 
 
 vis = xds[0].DATA.data
 visr = vis.rechunk((vis.chunks[0], args.chan_chunks, vis.chunks[2]))
 
+# Average each channel chunk to 1 channel
 avg = da.blockwise(fn, ("row", "chan", "corr"),
                    visr, ("row", "chan", "corr"),
                    adjust_chunks={"chan": 1},
@@ -32,3 +35,23 @@ avg = da.blockwise(fn, ("row", "chan", "corr"),
 
 print(avg.compute().shape)
 
+
+def fn2(data):
+    """ Because we're reducing over dimension chan
+    blockwise pass a list of numpy arrays to fn2 """
+    res = []
+
+    for element in data:
+        res.append(np.average(element, axis=1))
+
+    # numpy unfunc that sums the list of results together
+    return np.add.reduce(res)
+
+
+# Completely reduce over the channel dimension
+avg = da.blockwise(fn2, ("row", "corr"),
+                   visr, ("row", "chan", "corr"),
+                   dtype=visr.dtype)
+
+
+print(avg.compute().shape)
