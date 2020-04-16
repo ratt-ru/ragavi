cf9fd14ee38eedc236ec45623d6e5e6c6f069960
Lexy Andati <andatilexy@gmail.com>
2020-04-16 14:22:16 +0200

    Change default canvas size to 1080 x 720 Some organization

--- ragavi/arguments.py
+++ ragavi/arguments.py
@@ -14,20 +14,22 @@ def vis_argparser():
 
     """
     x_choices = ['ant', 'antenna', 'ant1', 'antenna1', 'ant2', 'antenna2',
-                 'chan', 'channel', 'freq',  'frequency', 'phase',
-                 'real', 'scan', 'time', 'uvdist', 'UVdist',
-                 'uvdistance', 'uvdistl', 'uvdist_l', 'UVwave', 'uvwave']
+                 'amp', 'amplitude', 'chan', 'channel', 'freq',  'frequency',
+                 'imag', 'imaginary', 'phase', 'real', 'scan', 'time',
+                 'uvdist', 'UVdist', 'uvdistance', 'uvdistl', 'uvdist_l',
+                 'UVwave', 'uvwave']
 
     y_choices = ['amp', 'amplitude', 'imag', 'imaginary', 'phase', 'real']
 
-    iter_choices = ["ant1", "antenna1", "ant2", "antenna2", "bl", "baseline",
+    iter_choices = ["ant", "antenna", "ant1", "antenna1", "ant2", "antenna2",
+                    "bl", "baseline",
                     "chan", "corr", "field", "scan", "spw"]
 
     parser = ArgumentParser(usage='ragavi-vis [options] <value>')
 
     required = parser.add_argument_group('Required arguments')
     required.add_argument('--ms', dest='mytabs',
-                          nargs='*', type=str, metavar='',
+                          nargs='+', type=str, metavar='',
                           help="MS to plot. Default is None",
                           default=[])
     required.add_argument('-x', '--xaxis', dest='xaxis', type=str, metavar='',
@@ -38,21 +40,37 @@ def vis_argparser():
                           default=None)
     parser.add_argument('-ch', '--canvas-height', dest='c_height', type=int,
                         metavar='',
-                        help="""Set height resulting image. Note: This is not the plot height. Default is 1020""",
-                        default=1020)
+                        help="""Set height resulting image. Note: This is not 
+                        the plot height. Default is 720""",
+                        default=720)
     parser.add_argument('-cw', '--canvas-width', dest='c_width', type=int,
                         metavar='',
-                        help="""Set width of the resulting image. Note: This is not the plot width. Default is 1720.""",
-                        default=1280)
+                        help="""Set width of the resulting image. Note: This 
+                        is not the plot width. Default is 1080.""",
+                        default=1080)
     parser.add_argument('--cbin', dest='cbin', type=int, metavar='',
-                        help="""Size of channel bins over which to average .e.g setting this to 50 will average over every 5 channels""",
+                        help="""Size of channel bins over which to average 
+                        .e.g setting this to 50 will average over every 5 
+                        channels""",
                         default=None)
     parser.add_argument('--chan', dest='chan', type=str, metavar='',
-                        help="""Channels to select. Can be specified using syntax i.e "0:5" (exclusive range) or "20" for channel 20 or "10~20" (inclusive range) (same as 10:21) "::10" for every 10th channel or "0,1,3" etc. Default is all.""",
+                        help="""Channels to select. Can be specified using 
+                        syntax i.e "0:5" (exclusive range) or "20" for 
+                        channel 20 or "10~20" (inclusive range) (same as 
+                        10:21) "::10" for every 10th channel or "0,1,3" etc. 
+                        Default is all.""",
                         default=None)
     parser.add_argument('-cs', '--chunks', dest='chunks', type=str,
                         metavar='',
-                        help="""Chunk sizes to be applied to the dataset. Can be an integer e.g "1000", or a comma separated string e.g "1000,100,2" for multiple dimensions. The available dimensions are (row, chan, corr) respectively. If an integer, the specified chunk size will be applied to all dimensions. If comma separated string, these chunk sizes will be applied to each dimension respectively. Default is 100,000 in the row axis.""",
+                        help="""Chunk sizes to be applied to the dataset. Can
+                         be an integer e.g "1000", or a comma separated 
+                         string e.g "1000,100,2" for multiple dimensions. 
+                         The available dimensions are (row, chan, corr) 
+                         respectively. If an integer, the specified chunk 
+                         size will be applied to all dimensions. If comma 
+                         separated string, these chunk sizes will be applied 
+                         to each dimension respectively. Default is 100,000 
+                         in the row axis.""",
                         default=None)
     parser.add_argument('--cmap', dest='mycmap', type=str, metavar='',
                         help="""Colour or colour map to use.A list of valid cmap arguments can be found at: 
@@ -66,24 +84,39 @@ def vis_argparser():
                         choices=iter_choices,
                         help="""Select column to colourise by. This will result in a single image. To generate grid, use argument '--iter-axis' Default is None.""",
                         default=None)
+    parser.add_argument('--cols', dest="n_cols", type=int, metavar='',
+                        help="""Number of columns if iteration is active.
+                        Default is 9.""",
+                        default=9)
     parser.add_argument('-c', '--corr', dest='corr', type=str, metavar='',
-                        help="""Correlation index or subset to plot. Can be specified using normal python slicing syntax i.e "0:5" for 0<=corr<5 or "::2" for every 2nd corr or "0" for corr 0  or "0,1,3". Default is all.""",
+                        help="""Correlation index or subset to plot. Can be 
+                        specified using normal python slicing syntax i.e 
+                        "0:5" for 0<=corr<5 or "::2" for every 2nd corr or 
+                        "0" for corr 0  or "0,1,3". Default is all.""",
                         default='0:')
     parser.add_argument('-dc', '--data-column', dest='data_column', type=str,
                         metavar='',
                         help="""MS column to use for data. Default is DATA.""", default='DATA')
     parser.add_argument('--ddid', dest='ddid', type=str,
                         metavar='',
-                        help="""DATA_DESC_ID(s) /spw to select. Can be specified as e.g. "5", "5,6,7", "5~7" (inclusive range), "5:8" (exclusive range), 5:(from 5 to last). Default is all.""",
+                        help="""DATA_DESC_ID(s) /spw to select. Can be 
+                        specified as e.g. "5", "5,6,7", "5~7" (inclusive 
+                        range), "5:8" (exclusive range), 5:(from 5 to last). 
+                        Default is all.""",
                         default=None)
     parser.add_argument('-f', '--field', dest='fields', type=str,
                         metavar='',
-                        help="""Field ID(s) / NAME(s) to plot. Can be specified as "0", "0,2,4", "0~3" (inclusive range), "0:3" (exclusive range), "3:" (from 3 to last) or using a field name or comma separated field names. Default is all""",
+                        help="""Field ID(s) / NAME(s) to plot. Can be 
+                        specified as "0", "0,2,4", "0~3" (inclusive range), 
+                        "0:3" (exclusive range), "3:" (from 3 to last) or 
+                        using a field name or comma separated field names. 
+                        Default is all""",
                         default=None)
-    parser.add_argument('-ia', '--iter-axis', dest='iterate', type=str,
+    parser.add_argument('-ia', '--iter-axis', dest='iter_axis', type=str,
                         metavar='',
                         choices=iter_choices,
-                        help="""Select column to iterate over. This will result in a grid. Default is None.""",
+                        help="""Select column to iterate over. This will 
+                        result in a grid. Default is None.""",
                         default=None)
     parser.add_argument('-o', '--htmlname', dest='html_name', type=str,
                         metavar='',
@@ -91,15 +124,17 @@ def vis_argparser():
     parser.add_argument('-ml', '--mem-limit', dest="mem_limit",
                         type=str, metavar='',
                         default="1GB",
-                        help="""Memory limit per core e.g '1GB' or '128MB' Default is 1GB""")
-    # NOTE: Changed iterate argument to colorize. Iteration to be added later
+                        help="""Memory limit per core e.g '1GB' or '128MB'.
+                         Default is 1GB""")
     parser.add_argument('-nf', '--no-flagged', dest='flag',
                         action='store_false',
-                        help="""Whether to plot both flagged and unflagged data. Default only plot data that is not flagged.""",
+                        help="""Whether to plot both flagged and unflagged 
+                        data. Default only plot data that is not flagged.""",
                         default=True)
     parser.add_argument('-nc', '--num-cores', dest='n_cores', type=int,
                         metavar='',
-                        help="""Number of CPU cores to be used by Dask. Default is half of the available cores""",
+                        help="""Number of CPU cores to be used by Dask. 
+                        Default is half of the available cores""",
                         default=int(cpu_count() / 2))
     parser.add_argument('-s', '--scan', dest='scan', type=str, metavar='',
                         help='Scan Number to select. Default is all.',
@@ -107,7 +142,9 @@ def vis_argparser():
     parser.add_argument('--taql', dest='where', type=str, metavar='',
                         help='TAQL where', default=None)
     parser.add_argument('--tbin', dest='tbin', type=float, metavar='',
-                        help="""Time in seconds over which to average .e.g setting this to 120.0 will average over every 120.0 seconds""",
+                        help="""Time in seconds over which to average .e.g 
+                        setting this to 120.0 will average over every 120.0 
+                        seconds""",
                         default=None)
     parser.add_argument('--xmin', dest='xmin', type=float, metavar='',
                         help='Minimum x value to plot', default=None)
@@ -153,7 +190,9 @@ def gains_argparser():
                         comma-separated list of antennas. Defaults to all.""",
                         default=None)
     parser.add_argument('-c', '--corr', dest='corr', type=str, metavar=' ',
-                        help="""Correlation index to plot. Can be a single integer or comma separated integers e.g '0,2'. Defaults to all.""",
+                        help="""Correlation index to plot. Can be a single 
+                        integer or comma separated integers e.g '0,2'. 
+                        Defaults to all.""",
                         default=None)
     parser.add_argument('--cmap', dest='mycmap', type=str, metavar=' ',
                         help="""Bokeh or Colorcet colour map to use for antennas. List of available colour maps can be 
@@ -162,15 +201,21 @@ def gains_argparser():
                         Defaults to coolwarm""",
                         default='coolwarm')
     parser.add_argument('--ddid', dest='ddid', type=str, metavar=' ',
-                        help="""SPECTRAL_WINDOW_ID or ddid number. Defaults to all""",
+                        help="""SPECTRAL_WINDOW_ID or ddid number.
+                         Defaults to all""",
                         default=None)
     parser.add_argument('-d', '--doplot', dest='doplot', type=str,
                         metavar=' ',
-                        help="""Plot complex values as amplitude & phase (ap) or real and imaginary (ri). Defaults to ap.""",
+                        help="""Plot complex values as amplitude & phase (ap) 
+                        or real and imaginary (ri). Defaults to ap.""",
                         default='ap')
     parser.add_argument('-f', '--field', dest='fields', type=str, nargs='+',
                         metavar='',
-                        help="""Field ID(s) / NAME(s) to plot. Can be specified as "0", "0,2,4", "0~3" (inclusive range), "0:3" (exclusive range), "3:" (from 3 to last) or using a field name or comma separated field names. Defaults to all""",
+                        help="""Field ID(s) / NAME(s) to plot. Can be 
+                        specified as "0", "0,2,4", "0~3" (inclusive range), 
+                        "0:3" (exclusive range), "3:" (from 3 to last) or 
+                        using a field name or comma separated field names. 
+                        Defaults to all""",
                         default=None)
     parser.add_argument('--htmlname', dest='html_name', type=str, metavar=' ',
                         help='Output HTML file name', default='')
@@ -184,10 +229,12 @@ def gains_argparser():
                         metavar=' ', help='Output PNG or SVG image name',
                         default='')
     parser.add_argument('--t0', dest='t0', type=float, metavar=' ',
-                        help="""Minimum time to plot [in seconds]. Defaults to full range]""",
+                        help="""Minimum time to plot [in seconds]. 
+                        Defaults to full range]""",
                         default=0)
     parser.add_argument('--t1', dest='t1', type=float, metavar=' ',
-                        help="""Maximum time to plot [in seconds]. Defaults to full range""",
+                        help="""Maximum time to plot [in seconds]. 
+                        Defaults to full range""",
                         default=None)
     parser.add_argument('--taql', dest='where', type=str, metavar=' ',
                         help='TAQL where clause',
