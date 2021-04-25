b580955fdd5fff133848b869bf0291b75aed0b8d
mulan-94 <andatilexy@gmail.com>
2021-04-25 12:58:25 +0200

    Add table not found and empty table exceptions

--- chaos/exceptions.py
+++ chaos/exceptions.py
@@ -1,8 +1,15 @@
 class ExRag(Exception):
     pass
 
+class EmptyTable(Exception):
+    pass
+
+class TableNotFound(Exception):
+    pass
+
 class InvalidCmap(Exception):
     pass
 
 class InvalidColumnName(Exception):
     pass
+
