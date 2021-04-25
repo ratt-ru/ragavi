e058f4c0f1f82167b640f78940a45aa4268be576
mulan-94 <andatilexy@gmail.com>
2021-04-25 12:59:10 +0200

    Change set_multi_defaults to a function from a class method

--- chaos/overrides.py
+++ chaos/overrides.py
@@ -1,6 +1,6 @@
 from collections import UserDict
 
-class rdict(UserDict):
+class rdict(dict):
     """Add a function to set multiple keys' defaults if they don't already exist"""
     def __repr__(self):
         return super().__repr__()
@@ -18,4 +18,11 @@ class rdict(UserDict):
             kwargs = args[0]
         for key, value in kwargs.items():
             self.data.setdefault(key, value)
+        
 
+
+def set_multiple_defaults(in_dict, defaults):
+    for key, value in defaults.items():
+        in_dict.setdefault(key, value)
+    
+    return in_dict
