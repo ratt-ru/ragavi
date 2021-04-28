631f7e57b82d04d4e806f348e144732eafdfe377
mulan-94 <andatilexy@gmail.com>
2021-04-28 12:19:16 +0200

    Add status table Fix extra axis adder

--- chaos/gains.py
+++ chaos/gains.py
@@ -16,7 +16,7 @@ from holy_chaos.chaos.ragdata import dataclass, field, MsData, Axargs, Genargs, 
 from holy_chaos.chaos.arguments import gains_argparser
 from holy_chaos.chaos.plotting import FigRag, Circle, Scatter
 from holy_chaos.chaos.processing import Chooser, Processor
-from holy_chaos.chaos.widgets import f_marks, make_widgets
+from holy_chaos.chaos.widgets import f_marks, make_widgets, make_stats_table
 
 from ipdb import set_trace
 
@@ -27,9 +27,10 @@ class Pargs(Plotargs):
 def get_table(msdata, sargs, group_data):
     # perform  TAQL selections and chan and corr selection
     super_taql = Chooser.form_taql_string(msdata, antennas=sargs.antennas,
-                                          baselines=sargs.baselines, fields=sargs.fields,
-                                          spws=sargs.ddids,
-                                          taql=sargs.taql, time=f"{sargs.t0}, {sargs.t1}")
+                                          baselines=sargs.baselines,
+                                          fields=sargs.fields,
+                                          spws=sargs.ddids, taql=sargs.taql,
+                                          time=(sargs.t0, sargs.t1))
     msdata.taql_selector = super_taql
 
     print(f"Selection string: {super_taql}")
@@ -43,12 +44,20 @@ def get_table(msdata, sargs, group_data):
                                  })
 
     for i, sub in enumerate(mss):
-        mss[i] = sub.sel(chan=Chooser.get_knife(sargs.channels),
-                corr=Chooser.get_knife(sargs.corrs))
+        mss[i] = sub.sel(corr=sargs.corrs)
     return mss
 
-def calculate_stats(msdata, yaxis):
-    pass
+def add_extra_xaxis(msdata, figrag, sargs):
+    channels = msdata.freqs.sel(chan=sargs.channels)
+    for sp in msdata.active_spws:
+        if channels.ndim > 1:
+            chans = channels.sel(row=sp)
+        else:
+            chans = channels
+        chans = chans[[0, -1]].values/1e9
+        figrag.add_axis(chans[0], chans[-1], "x",
+                        "linear", "Frequency GHz", "above")
+    
 
 def iron_data(ax_info):
     """Flatten these data to 1D"""
@@ -64,6 +73,10 @@ def iron_data(ax_info):
     ax_info.xdata = np.tile(ax_info.xdata, 
             ax_info.ydata.size//ax_info.xdata.size)
     return ax_info
+
+def hovertool_data():
+    """spw, field, scan, corr, antenna"""
+    pass
 
 def get_colours(n, cmap="coolwarm"):
     """
@@ -97,17 +110,32 @@ def get_colours(n, cmap="coolwarm"):
         raise InvalidCmap(f"cmap {cmap} not found.")
 
 
+#"/home/lexya/Documents/test_gaintables/1491291289.B0"
+#"/home/lexya/Documents/test_gaintables/1491291289.F0"
+#"/home/lexya/Documents/test_gaintables/1491291289.K0"
+#"/home/lexya/Documents/test_gaintables/1491291289.G0"
+#"/home/lexya/Documents/test_gaintables/3c391_ctm_mosaic_10s_spw0.B0"
+#"/home/lexya/Documents/test_gaintables/4k_pcal3_1-1576599977_fullpol_1318to1418MHZ-single-3pcal_1.Df"
+#"/home/lexya/Documents/test_gaintables/4k_pcal3_1-1576599977_fullpol_1318to1418MHZ-single-3pcal_1.Kcrs"
+#"/home/lexya/Documents/test_gaintables/4k_pcal3_1-1576599977_fullpol_1318to1418MHZ-single-3pcal_1.Xf"
+#"/home/lexya/Documents/test_gaintables/4k_pcal3_1-1576599977_fullpol_1318to1418MHZ-single-3pcal_1.Xref"
+#"/home/lexya/Documents/test_gaintables/J0538_floi-beck-1pcal.Xfparang"
+#"/home/lexya/Documents/test_gaintables/short-gps1-1gc1_primary_cal.G0"
+#"/home/lexya/Documents/test_gaintables/VLBA_0402_3C84.B0"
+#"/home/lexya/Documents/test_gaintables/workflow2-1576687564_sdp_l0-1gc1_primary.B0"
+
+
 if __name__ == "__main__":
-    gargs = ["-t", "/home/lexya/Downloads/radioastro/tutorial_docs/calibration_tutorial/1491291289.G0", "b.ms", "c.ms",
+    gargs = ["-t", "/home/lexya/Documents/test_gaintables/1491291289.B0", "b.ms", "c.ms",
              "--cmap", "coolwarm", 
-             "-a", "", "m000,m10,m063", "7",
-             "-c", "", "1", "0,1",
-             "--ddid", "0",
-             "-f", "", "DEEP2", "J342-342,X034-123",
-             "--t0", "0", "1000", "2000",
-             "--t1", "10000", "7000", "7500",
-             "-y", "a,p,r", "r,i", "i,a",
-             "-x", "time", "time", "chan"]
+             "-a", "m000", "m000,m10,m063", "7",
+            #  "-c", "", "1", "0,1",
+            #  "--ddid", "0",
+            #  "-f", "", "DEEP2", "J342-342,X034-123",
+            #  "--t0", "0", "1000", "2000",
+            #  "--t1", "10000", "7000", "7500",
+             "-y", "a", "r,i", "i,a",
+             "-x", "channel", "time", "chan"]
 
     ps = gains_argparser().parse_args(gargs)
 
@@ -126,15 +154,15 @@ else:
 
 ps.cmaps = ps.cmaps * len(ps.msnames) if len(ps.cmaps)==1 else ps.cmaps
 
-
 for (msname, antennas, baselines, channels, corrs, ddids, fields, t0, t1, taql, cmap,
     yaxes, xaxis)in zip_longest(ps.msnames, ps.antennas, ps.baselines, ps.channels, ps.corrs, ps.ddids,
     ps.fields, ps.t0s, ps.t1s, ps.taqls, ps.cmaps, ps.yaxes, ps.xaxes):
 
     #we're grouping the arguments into 4
     gen_args = Genargs(msname=msname, version="testwhatever")
-    sel_args = Selargs(antennas=antennas, corrs=corrs, baselines=baselines,
-                        channels=channels, ddids=ddids, fields=fields, taql=taql, t0=t0, t1=t1)
+    sel_args = Selargs(antennas=antennas, corrs=Chooser.get_knife(corrs),
+                       baselines=baselines, channels=Chooser.get_knife(channels),
+                       ddids=ddids, fields=fields, taql=taql, t0=t0, t1=t1)
     
     #initialise data ssoc with ms
     msdata = MsData(msname)
@@ -152,7 +180,7 @@ for (msname, antennas, baselines, channels, corrs, ddids, fields, t0, t1, taql, 
         raise EmptyTable("Table came up empty. Please your check selection.")
     
     all_figs = []
-    
+        
     for yaxis in yaxes.split(","):
         print(f"Axis: {yaxis}")
 
@@ -160,9 +188,9 @@ for (msname, antennas, baselines, channels, corrs, ddids, fields, t0, t1, taql, 
             x_scale=xaxis if xaxis == "time" else "linear" )
 
         for sub in subs:
-            print(f"Antenna {sub.ANTENNA1}")
+            # print(f"Antenna {sub.ANTENNA1}")
             msdata.active_channels = msdata.freqs.sel(
-                chan=Chooser.get_knife(sel_args.channels),
+                chan=sel_args.channels,
                 row=sub.SPECTRAL_WINDOW_ID)
             
             if sub.FIELD_ID not in msdata.active_fields:
@@ -172,9 +200,8 @@ for (msname, antennas, baselines, channels, corrs, ddids, fields, t0, t1, taql, 
 
             ax_info = Axargs(xaxis=xaxis, yaxis=yaxis, data_column="CPARAM",
                                                    ms_obj=sub, msdata=msdata)
-            
-            ax_info.xdata = Processor(
-                ax_info.xdata).calculate(ax_info.xaxis).data
+            ax_info.xdata = Processor(ax_info.xdata).calculate(
+                ax_info.xaxis).data
 
             for corr in msdata.active_corrs:
                 # setting ydata here for reinit. Otherwise, autoset in axargs
@@ -194,24 +221,27 @@ for (msname, antennas, baselines, channels, corrs, ddids, fields, t0, t1, taql, 
                           f"s{sub.SPECTRAL_WINDOW_ID}",
                           f"c{corr}", f"f{sub.FIELD_ID}"])
 
-        figrag.update_xlabel(ax_info.xaxis.capitalize())
-        figrag.update_ylabel(ax_info.yaxis.capitalize())
-        figrag.update_title(
-            f"{ax_info.yaxis.capitalize()} vs {ax_info.xaxis.capitalize()}")
+        figrag.update_xlabel(ax_info.xaxis)
+        figrag.update_ylabel(ax_info.yaxis)
+        
+        if "chan" in ax_info.xaxis:
+            add_extra_xaxis(msdata, figrag, sel_args)
+        
         figrag.add_legends(group_size=8, visible=True)
+        figrag.update_title(f"{ax_info.yaxis} vs {ax_info.xaxis}")
         figrag.show_glyphs(selection="b0")
         
         all_figs.append(figrag.fig)
         widgets = make_widgets(msdata, all_figs[0], group_size=8)
-        output_file(filename = "oster.html")
-    save(column(row(widgets), *all_figs),filename="oster.html", title="oster")
+        stats = make_stats_table(msdata, ax_info.data_column, yaxes,
+                get_table(msdata, sel_args, group_data=["SPECTRAL_WINDOW_ID",
+                                                        "FIELD_ID"]))
+        output_file(filename = "ghost.html")
+    save(column(row(widgets), stats, *all_figs),filename="ghost.html", title="oster")
 
 
         # figrag.write_out()
-        # set_trace()
     # add the widgets at this point. Only need the first figure
     set_trace()
-    make_widgets(msdata, all_figs[0], group_size=8)
-
-
+    print("Ends")
 
